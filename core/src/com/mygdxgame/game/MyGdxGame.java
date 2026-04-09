ec5f3dc7e40c53eaf4fc001990d9ce7d3f1ff80b
leledame <leledame@ya.ru>
2026-04-09 17:13:14 +0300

    2nd commit

--- core/src/com/mygdxgame/game/MyGdxGame.java
+++ core/src/com/mygdxgame/game/MyGdxGame.java
@@ -1,33 +1,35 @@
 package com.mygdxgame.game;
 
-// Импорт модулей из фреймворка
-
-import static com.mygdxgame.game.screens.GameScreen.POSITION_ITERATIONS;
-import static com.mygdxgame.game.screens.GameScreen.STEP_TIME;
-import static com.mygdxgame.game.screens.GameScreen.VELOCITY_ITERATIONS;
-
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Box2D;
 import com.badlogic.gdx.physics.box2d.World;
 import com.mygdxgame.game.screens.GameScreen;
 
+import static com.mygdxgame.game.GameSettings.*;
+
 public class MyGdxGame extends Game {
 
-    public SpriteBatch batch;
     public World world;
+
+    public Vector3 touch;
+    public SpriteBatch batch;
     public OrthographicCamera camera;
 
     public GameScreen gameScreen;
+
     float accumulator = 0;
 
     @Override
     public void create() {
+
         Box2D.init();
         world = new World(new Vector2(0, 0), true);
+
         batch = new SpriteBatch();
         camera = new OrthographicCamera();
         camera.setToOrtho(false, GameSettings.SCREEN_WIDTH, GameSettings.SCREEN_HEIGHT);
@@ -37,18 +39,18 @@ public class MyGdxGame extends Game {
         setScreen(gameScreen);
     }
 
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
+
     public void stepWorld() {
         float delta = Gdx.graphics.getDeltaTime();
-        accumulator += delta;
+        accumulator += Math.min(delta, 0.25f);
 
         if (accumulator >= STEP_TIME) {
             accumulator -= STEP_TIME;
             world.step(STEP_TIME, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
         }
-    }
-
-    @Override
-    public void dispose() {
-        batch.dispose();
     }
 }
--- core/src/com/mygdxgame/game/screens/GameScreen.java
+++ core/src/com/mygdxgame/game/screens/GameScreen.java
@@ -1,51 +1,55 @@
 package com.mygdxgame.game.screens;
 
-import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.ScreenUtils;
 import com.mygdxgame.game.GameResources;
 import com.mygdxgame.game.GameSettings;
 import com.mygdxgame.game.MyGdxGame;
+import com.mygdxgame.game.objects.ShipObject;
 
-public class GameScreen implements Screen {
+public class GameScreen extends ScreenAdapter {
+
     MyGdxGame myGdxGame;
-
-
-
-    public static final float STEP_TIME = 1f / 60;
-    public static final int VELOCITY_ITERATIONS = 6;
-    public static final int POSITION_ITERATIONS = 6;
-
+    ShipObject shipObject;
 
 
     public GameScreen(MyGdxGame myGdxGame) {
         this.myGdxGame = myGdxGame;
-    }
 
-    @Override
-    public void show() {
+        shipObject = new ShipObject(
+                GameSettings.SCREEN_WIDTH / 2, 150,
+                GameSettings.SHIP_WIDTH, GameSettings.SHIP_HEIGHT,
+                GameResources.SHIP_IMG_PATH,
+                myGdxGame.world
+        );
     }
 
     @Override
     public void render(float delta) {
+
+        myGdxGame.stepWorld();
+        handleInput();
+        draw();
     }
 
-    @Override
-    public void resize(int width, int height) {
+    private void handleInput() {
+        if (Gdx.input.isTouched()) {
+            myGdxGame.touch = myGdxGame.camera.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
+            shipObject.move(myGdxGame.touch);
+        }
     }
 
-    @Override
-    public void pause() {
-    }
+    private void draw() {
 
-    @Override
-    public void resume() {
-    }
+        myGdxGame.camera.update();
+        myGdxGame.batch.setProjectionMatrix(myGdxGame.camera.combined);
+        ScreenUtils.clear(Color.CLEAR);
 
-    @Override
-    public void hide() {
+        myGdxGame.batch.begin();
+        shipObject.draw(myGdxGame.batch);
+        myGdxGame.batch.end();
     }
-
-    @Override
-    public void dispose() {
-    }
-
 }
